4200a9653e2628bb48e3b8672d59a1fea338c37d
Vanessa <vanessalins356@gmail.com>
2025-03-17 19:29:56 -0300

    primeiro

---
+++ publicar/src/main/java/com/example/publicar/PublicarApplication.java
@@ -0,0 +1,13 @@
+package com.example.publicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicarApplication.class, args);
+	}
+
+}
---
+++ publicar/src/test/java/com/example/publicar/PublicarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
