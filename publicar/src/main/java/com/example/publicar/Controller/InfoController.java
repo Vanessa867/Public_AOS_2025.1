5f9864bb0f7ffdbc02b807da4657470501ee7297
Vanessa <vanessalins356@gmail.com>
2025-03-30 17:27:54 -0300

    resttemplate

---
+++ publicar/src/main/java/com/example/publicar/Controller/InfoController.java
@@ -0,0 +1,19 @@
+package com.example.publicar.Controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class InfoController {
+    @GetMapping("/info")
+    public String getInfo() {
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl = "https://dog.ceo/api/breeds/image/random";
+        String response = restTemplate.getForObject(apiUrl, String.class);
+        return response;
+    }
+}
