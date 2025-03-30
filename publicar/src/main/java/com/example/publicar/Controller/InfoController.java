9f2111ad6932c1f60ef7a4df6aa4f8e7303dbb1b
Vanessa <vanessalins356@gmail.com>
2025-03-30 19:06:46 -0300

    atual

--- publicar/src/main/java/com/example/publicar/Controller/InfoController.java
+++ publicar/src/main/java/com/example/publicar/Controller/InfoController.java
@@ -4,18 +4,21 @@ package com.example.publicar.Controller;
 import lombok.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("/info")
 @CrossOrigin(origins = "*")
 public class InfoController {
 
-    @GetMapping("/info")
-    public String getInfo() {
+    @GetMapping
+    public Map<String, Object> getInfo() {
         RestTemplate restTemplate = new RestTemplate();
         String apiUrl = "https://dog.ceo/api/breeds/image/random";
-        String response = restTemplate.getForObject(apiUrl, String.class);
-        return response;
+        return restTemplate.getForObject(apiUrl, Map.class);
     }
 }
